579afd34e18ce013dbdd656ea535d43d9f1a65bc
Unknown <mateo2096@hotmail.es>
2018-08-26 09:51:27 -0500

    Project Created

---
+++ src/main/java/org/grandma/grandmaschedule/GrandmaScheduleApplication.java
@@ -0,0 +1,12 @@
+package org.grandma.grandmaschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrandmaScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrandmaScheduleApplication.class, args);
+	}
+}
---
+++ src/test/java/org/grandma/grandmaschedule/GrandmaScheduleApplicationTests.java
@@ -0,0 +1,16 @@
+package org.grandma.grandmaschedule;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GrandmaScheduleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
