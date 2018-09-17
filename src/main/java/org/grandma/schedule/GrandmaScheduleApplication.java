57bc8590d14b5537faa97148cc5388045c5e57d0
Mateo Rojas <mateo2096@hotmail.es>
2018-09-16 23:02:19 -0500

    Adds configuration for connecting to the database

--- src/main/java/org/grandma/schedule/GrandmaScheduleApplication.java
+++ src/main/java/org/grandma/schedule/GrandmaScheduleApplication.java
@@ -1,9 +1,16 @@
 package org.grandma.schedule;
 
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.web.client.RestTemplate;
+
+import javax.sql.DataSource;
+import java.util.Properties;
 
 @SpringBootApplication
 public class GrandmaScheduleApplication {
@@ -15,5 +22,40 @@ public class GrandmaScheduleApplication {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory(DataSource dataSource) {
+
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setPackagesToScan(new String[] {"org.grandma.schedule.model"});
+        sessionFactory.setHibernateProperties(new Properties(){
+            {
+                setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+                setProperty("hibernate.show_sql", "true");
+            }
+        });
+
+        return sessionFactory;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost/hospital?serverTimezone=UTC&useSSL=false");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
+
+        return dataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager hibernateTransactionManager(LocalSessionFactoryBean sessionFactory) {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory.getObject());
+        return transactionManager;
     }
 }
