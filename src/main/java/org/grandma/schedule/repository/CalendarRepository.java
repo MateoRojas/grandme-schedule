c2e9ce46db6e8d038c59ac7d0e314d49f868b30f
Mateo Rojas <mateo2096@hotmail.es>
2018-09-16 22:19:19 -0500

    Removes spring data and add querydsl

--- src/main/java/org/grandma/schedule/repository/CalendarRepository.java
+++ src/main/java/org/grandma/schedule/repository/CalendarRepository.java
@@ -1,13 +1,15 @@
 package org.grandma.schedule.repository;
 
-import org.grandma.schedule.model.Calendar;
 import org.grandma.schedule.vo.PersonMessage;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.Collection;
 
-public interface CalendarRepository extends CrudRepository<Calendar, Integer> {
+@Repository
+public class CalendarRepository {
 
-    Collection<PersonMessage> findByDate(LocalDate date);
+    public Collection<PersonMessage> findByDate(LocalDate date) {
+        return null;
+    }
 }
