e31fc9a432e49531091e0aa6051f3063721cc1f5
Mateo Rojas <mateo2096@hotmail.es>
2018-09-22 23:15:34 -0500

    Moves persistence related beans and properties to their own files

--- src/main/java/org/grandma/schedule/GrandmaScheduleApplication.java
+++ src/main/java/org/grandma/schedule/GrandmaScheduleApplication.java
@@ -1,21 +1,11 @@
 package org.grandma.schedule;
 
-import com.querydsl.jpa.hibernate.HibernateQueryFactory;
-import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
 
-import javax.sql.DataSource;
-import java.util.Properties;
-
 @SpringBootApplication
-@EnableTransactionManagement
 public class GrandmaScheduleApplication {
 
     public static void main(String[] args) {
@@ -25,45 +15,5 @@ public class GrandmaScheduleApplication {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
-    }
-
-    @Bean
-    public HibernateQueryFactory queryFactory(LocalSessionFactoryBean sessionFactory) {
-        return new HibernateQueryFactory(() -> sessionFactory.getObject().getCurrentSession());
-    }
-
-    @Bean
-    public LocalSessionFactoryBean sessionFactory(DataSource dataSource) {
-
-        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-        sessionFactory.setDataSource(dataSource);
-        sessionFactory.setPackagesToScan("org.grandma.schedule.dto");
-        sessionFactory.setHibernateProperties(new Properties(){
-            {
-                setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
-                setProperty("hibernate.show_sql", "true");
-            }
-        });
-
-        return sessionFactory;
-    }
-
-    @Bean
-    public DataSource dataSource() {
-
-        BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost/grandma-schedule-db?serverTimezone=UTC");
-        dataSource.setUsername("grandma-schedule-db");
-        dataSource.setPassword("12345678");
-
-        return dataSource;
-    }
-
-    @Bean
-    public PlatformTransactionManager hibernateTransactionManager(LocalSessionFactoryBean sessionFactory) {
-        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-        transactionManager.setSessionFactory(sessionFactory.getObject());
-        return transactionManager;
     }
 }
---
+++ src/main/java/org/grandma/schedule/config/PersistanceConfig.java
@@ -0,0 +1,66 @@
+package org.grandma.schedule.config;
+
+import com.querydsl.jpa.hibernate.HibernateQueryFactory;
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+@PropertySource("persistence.properties")
+public class PersistanceConfig {
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public HibernateQueryFactory queryFactory(LocalSessionFactoryBean sessionFactory) {
+        return new HibernateQueryFactory(() -> sessionFactory.getObject().getCurrentSession());
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory(DataSource dataSource) {
+
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setPackagesToScan("org.grandma.schedule.dto");
+        sessionFactory.setHibernateProperties(new Properties(){
+            {
+                setProperty("hibernate.dialect", environment.getProperty("hibernate.dialect"));
+                setProperty("hibernate.show_sql", environment.getProperty("hibernate.show_sql"));
+            }
+        });
+
+        return sessionFactory;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(environment.getProperty("jdbc.driverClassName"));
+        dataSource.setUrl(environment.getProperty("jdbc.url"));
+        dataSource.setUsername(environment.getProperty("jdbc.user"));
+        dataSource.setPassword(environment.getProperty("jdbc.password"));
+
+        return dataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager hibernateTransactionManager(LocalSessionFactoryBean sessionFactory) {
+
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory.getObject());
+        return transactionManager;
+    }
+}
